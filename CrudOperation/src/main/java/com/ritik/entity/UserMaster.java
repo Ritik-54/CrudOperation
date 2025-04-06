54d005aecfd0f74dda586b4a877d5d3c5c40ab88
Ritik <ritik54@gmail.com>
2025-04-06 08:57:47 +0530

    Delete the changes variable from master table

--- CrudOperation/src/main/java/com/ritik/entity/UserMaster.java
+++ CrudOperation/src/main/java/com/ritik/entity/UserMaster.java
@@ -12,7 +12,6 @@ public class UserMaster {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	public long id;
 	public String username;
-	public String changes;
 	public String password;
 	public String status;
 	public String createdBy;
